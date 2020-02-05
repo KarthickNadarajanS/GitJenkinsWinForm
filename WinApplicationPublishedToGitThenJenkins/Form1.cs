2b0600ea2cb29d8ccb8e9414aa35d458e8d43a0b
Karthicknadarajan Selvaraj <Karthicknadarajan.Selvaraj@kla-tencor.com>
2020-02-05 19:50:01 +0530

    fiveagain

--- WinApplicationPublishedToGitThenJenkins/Form1.cs
+++ WinApplicationPublishedToGitThenJenkins/Form1.cs
@@ -19,7 +19,7 @@ namespace WinApplicationPublishedToGitThenJenkins
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Button1 Cli");
+            Console.WriteLine("Button1 Cl");
         }
 
         private void Form1_Load(object sender, EventArgs e)
