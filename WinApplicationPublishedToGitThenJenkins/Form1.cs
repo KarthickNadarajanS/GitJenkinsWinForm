eb63978f6345140a61140b59c0a110978975ca73
Karthicknadarajan Selvaraj <Karthicknadarajan.Selvaraj@kla-tencor.com>
2020-02-05 18:48:54 +0530

    Third Commit

--- WinApplicationPublishedToGitThenJenkins/Form1.cs
+++ WinApplicationPublishedToGitThenJenkins/Form1.cs
@@ -19,7 +19,7 @@ namespace WinApplicationPublishedToGitThenJenkins
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Button1 Clicked");
+            Console.WriteLine("Button1 Click");
         }
 
         private void Form1_Load(object sender, EventArgs e)
