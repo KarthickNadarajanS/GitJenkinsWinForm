bcab0cf1560513d4db0baf5f63e438671380bd26
KarthickNadarajanS <38006635+KarthickNadarajanS@users.noreply.github.com>
2020-02-06 14:46:14 +0530

    Update Form1.cs

--- WinApplicationPublishedToGitThenJenkins/Form1.cs
+++ WinApplicationPublishedToGitThenJenkins/Form1.cs
@@ -19,7 +19,7 @@ namespace WinApplicationPublishedToGitThenJenkins
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("Button1 ");
+            Console.WriteLine("Button1 C");
         }
 
         private void Form1_Load(object sender, EventArgs e)
